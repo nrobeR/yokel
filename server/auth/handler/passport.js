f13a06bd030c784ab6b65aebb4f1a1319afa0623
loqi <loqi@cruzio.com>
2014-09-09 11:06:11 -0700

    (cleanup) normalizes whitespace \t to '  ' and EOF with \n

--- server/auth/handler/passport.js
+++ server/auth/handler/passport.js
@@ -10,7 +10,7 @@ var configAuth = require('../../config/auth');
 
 module.exports = function(passport) {
 
-	// used to serialize the user for the session
+  // used to serialize the user for the session
   passport.serializeUser(function(user, done) {
       done(null, user.facebookID);
   });
@@ -26,15 +26,15 @@ module.exports = function(passport) {
       });
   });
     
-	// code for login (use('local-login', new LocalStategy))
-	// code for signup (use('local-signup', new LocalStategy))
+  // code for login (use('local-login', new LocalStategy))
+  // code for signup (use('local-signup', new LocalStategy))
 
-	// =========================================================================
+  // =========================================================================
     // FACEBOOK ================================================================
     // =========================================================================
     passport.use(new FacebookStrategy({
 
-		// pull in our app id and secret from our auth.js file
+    // pull in our app id and secret from our auth.js file
         clientID        : configAuth.facebookAuth.clientID,
         clientSecret    : configAuth.facebookAuth.clientSecret,
         callbackURL     : configAuth.facebookAuth.callbackURL
--- server/routes.js
+++ server/routes.js
@@ -37,23 +37,23 @@ module.exports = function(app, passport){
   });
 
     // =====================================
-	  //   FACEBOOK ROUTES =====================
-	  // =====================================
-	  // route for facebook authentication and login
-	  app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+    //   FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
 
-	  // handle the callback after facebook has authenticated the user
-	  app.get('/auth/facebook/callback',
-		  passport.authenticate('facebook', {
-			  successRedirect : '/',
-			  failureRedirect : '/'
-		  }));
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+      passport.authenticate('facebook', {
+        successRedirect : '/',
+        failureRedirect : '/'
+      }));
 
-	  // route for logging out
-	  app.get('/logout', function(req, res) {
-		  req.logout();
-		  res.redirect('/');
-	  });
+    // route for logging out
+    app.get('/logout', function(req, res) {
+      req.logout();
+      res.redirect('/');
+    });
 
 
   // All undefined asset or api routes should return a 404
@@ -70,11 +70,10 @@ module.exports = function(app, passport){
   // route middleware to make sure a user is logged in
   function isLoggedIn(req, res, next) {
 
-	  // if user is authenticated in the session, carry on
-	  if (req.isAuthenticated())
-		  return next();
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+      return next();
 
-	  // if they aren't redirect them to the home page
-	  res.redirect('/');
+    // if they aren't redirect them to the home page
+    res.redirect('/');
   }
-
